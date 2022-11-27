e078ec7f35faab90354d21437ff6d5b6f6c7fa88
Woosinyeong <tlsdud8205@naver.com>
2022-11-27 15:40:23 +0900

    Add main_calendar, Title text

--- app/src/main/java/com/sggyu/diet_app/MainActivity.java
+++ app/src/main/java/com/sggyu/diet_app/MainActivity.java
@@ -3,12 +3,18 @@ package com.sggyu.diet_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public CalendarView calendarView;
+    public ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calendarView = findViewById(R.id.calendarView);
     }
 }

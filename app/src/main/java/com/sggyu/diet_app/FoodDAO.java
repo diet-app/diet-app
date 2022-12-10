358e39c118bae89343f8876169334eaa39ecc58b
Woosinyeong <tlsdud8205@naver.com>
2022-12-10 17:17:09 +0900

    팝업창 DB연결, 총 칼로리 계산 추가

--- app/src/main/java/com/sggyu/diet_app/FoodDAO.java
+++ app/src/main/java/com/sggyu/diet_app/FoodDAO.java
@@ -13,6 +13,9 @@ public interface FoodDAO {
     @Query("SELECT * FROM food")
     List<Food> getAll();
 
+    @Query("SELECT * FROM food WHERE name =:str")
+    List<Food> getByName(String str);
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(Food... foods);
 
